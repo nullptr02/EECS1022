1d243a0fd2e1895cbfe193c79110393ea0300342
nullptr <michael@lablimited.ca>
2018-09-28 01:17:22 -0400

    First Commit

---
+++ Lab0/app/src/main/java/ca/yorku/eecs/zero/ZeroActivity.java
@@ -0,0 +1,15 @@
+package ca.yorku.eecs.zero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZeroActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zero);
+    }
+}
