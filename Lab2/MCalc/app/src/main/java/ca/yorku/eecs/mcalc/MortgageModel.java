da088b3fafdd44c1ce64459cdf19ea46ffb3539a
nullptr <michael@lablimited.ca>
2018-09-28 14:20:14 -0400

    Initial Commit

---
+++ Lab2/MCalc/app/src/main/java/ca/yorku/eecs/mcalc/MortgageModel.java
@@ -0,0 +1,15 @@
+package ca.yorku.eecs.mcalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MortgageModel extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mortgage_layout);
+    }
+}
