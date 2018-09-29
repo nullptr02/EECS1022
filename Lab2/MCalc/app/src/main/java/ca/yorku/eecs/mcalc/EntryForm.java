773ac546cd966f9eb72453b85941bab66479247e
nullptr <michael@lablimited.ca>
2018-09-28 23:47:45 -0400

    Second Commit

---
+++ Lab2/MCalc/app/src/main/java/ca/yorku/eecs/mcalc/EntryForm.java
@@ -0,0 +1,36 @@
+package ca.yorku.eecs.mcalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EntryForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mortgage_layout);
+    }
+
+
+
+    public void buttonClicked(View v) {
+
+
+        View principleView = findViewById(R.id.principleBox);
+        EditText principleEdit = (EditText) principleView;
+        String principle = principleEdit.getText().toString();
+        String amortization = ((EditText) findViewById(R.id.amortizationBox)).getText().toString();
+        String interest = ((EditText) findViewById(R.id.interestBox)).getText().toString();
+
+        MortgageModel model = new MortgageModel(principle, amortization, interest );
+        String answer = "$" + model.computePayment();
+
+        ((TextView) findViewById(R.id.answer)).setText(answer);
+
+
+    }
+
+}
--- Lab2/MCalc/app/src/main/java/ca/yorku/eecs/mcalc/MortgageModel.java
+++ Lab2/MCalc/app/src/main/java/ca/yorku/eecs/mcalc/MortgageModel.java
@@ -1,15 +1,43 @@
 package ca.yorku.eecs.mcalc;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.io.PrintStream;
+import java.util.Scanner;
 
-public class MortgageModel extends AppCompatActivity
-{
+public class MortgageModel {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mortgage_layout);
+    private double principle;
+    private double interest;
+    private double amortization;
+
+
+    public MortgageModel(String p, String a, String i) {
+
+        this.principle = Double.parseDouble(p);
+        this.interest = Double.parseDouble(i);
+        this.amortization = Double.parseDouble(a);
+
     }
+
+    public String computePayment() {
+        double interest = this.interest/(100*12);
+        double amortization = this.amortization*12;
+        double index = (this.principle*interest)/(1-(1/(1+(amortization*interest)+ amortization*(amortization-1)*Math.pow(interest,2)/2)));
+        String result = String.format("%,.2f", index);
+
+        return result;
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        PrintStream output = System.out;
+
+        MortgageModel model = new MortgageModel("700000", "25","2.75");
+        output.println(model.computePayment());
+
+    }
+
+
 }
